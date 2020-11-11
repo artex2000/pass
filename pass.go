5efbb343168e3fe39baef64b63a7d63e2b8911d0
Artem Shchygel <artems@ami.com>
2020-11-10 20:54:35 -0500

    Initial commit

---
+++ pass.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "bytes"
+    "strings"
+    "syscall"
+    "golang.org/x/crypto/ssh/terminal"
+)
+
+type Record struct {
+    nick string
+    hint string
+    pass []byte
+}
+
+func main() {
+    r := bufio.NewReader(os.Stdin)
+    db := make([]Record, 0)
+    for {
+        fmt.Print("Pass> ")
+        c, _ := r.ReadString('\n')
+        c = strings.TrimSpace(c)
+        if c == "add" {
+            var n, h string
+            fmt.Print("Nickname> ")
+            n, _ = r.ReadString('\n')
+            n = strings.TrimSpace(n)
+            fmt.Print("Hint> ")
+            h, _ = r.ReadString('\n')
+            h = strings.TrimSpace(h)
+            for i := 0; i < 3; i++ {
+                fmt.Print("Password> ")
+                p, _ := terminal.ReadPassword(int(syscall.Stdin))
+                fmt.Print("Repeat> ")
+                p2, _ := terminal.ReadPassword(int(syscall.Stdin))
+                if len(p) == 0 {
+                    fmt.Println("Password can't be empty")
+                } else if !bytes.Equal(p, p2) {
+                    fmt.Println("Passwords don't match")
+                } else {
+                    r := Record{n, h, p}
+                    db = append(db, r)
+                    break
+                }
+            }
+            continue
+        } else if c == "list" {
+            if len(db) == 0 {
+                fmt.Println("No records found")
+            } else {
+                for i, v := range db {
+                    fmt.Printf("%d. %s (%s)\n", i, v.nick, v.hint)
+                }
+            }
+            continue
+        } else if c == "quit" {
+            break
+        } else {
+            fmt.Printf("Unknown command: %s\n", c)
+        }
+    }
+}
+
